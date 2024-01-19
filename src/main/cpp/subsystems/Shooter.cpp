cf20f8a3c852c03c1f85f72166a4ba3354909e53
AAAAAPPP <[email protected]>
2024-01-18 19:23:24 -0500

    Shooter Update

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -6,7 +6,7 @@ m_motor2{ShooterConstant::Motor2ID, rev::CANSparkMax::MotorType::kBrushless},
 m_motor1Controller{m_motor1.GetPIDController()},
 m_motor2Controller{m_motor2.GetPIDController()}
 {
-m_motor1.SetInverted(true);
+m_motor2.SetInverted(true);
 }
 
 void Shooter::Periodic() {
