91aa7a15a910d6b9ff2bab3d1c89ddeb8d377195
ljalbert582 <55248950+ljalbert582@users.noreply.github.com>
2024-02-21 18:29:16 -0500

    Reessayer init CAN si code d'erreur pour barre.cpp
    Reste les autres sous-systemes a changere

--- src/main/cpp/subsystems/Barre.cpp
+++ src/main/cpp/subsystems/Barre.cpp
@@ -10,115 +10,132 @@ double computekAF1erJoint(double angle) {
 
 Barre::Barre()
     : m_MoteurPremierJoint{BarreConstant::moteurPremierJointID},
-      m_MoteurDeuxiemeJoint{BarreConstant::moteurDeuxiemeJointID} {
+      m_MoteurDeuxiemeJoint{BarreConstant::moteurDeuxiemeJointID}, m_MotorsInitialized{false} {
 
-    m_MoteurPremierJoint.ConfigFactoryDefault();
-    m_MoteurDeuxiemeJoint.ConfigFactoryDefault();
+    // Non-zero or false indicates success
+    if (m_MoteurPremierJoint.ConfigFactoryDefault() ||
+            m_MoteurDeuxiemeJoint.ConfigFactoryDefault() ||
 
-    m_MoteurPremierJoint.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
-    m_MoteurDeuxiemeJoint.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
+            m_MoteurPremierJoint.ConfigSelectedFeedbackSensor(
+                FeedbackDevice::CTRE_MagEncoder_Relative, 0, BarreConstant::kTimeoutMs) ||
+            m_MoteurDeuxiemeJoint.ConfigSelectedFeedbackSensor(
+                FeedbackDevice::CTRE_MagEncoder_Relative, 0, BarreConstant::kTimeoutMs) ||
 
-    m_MoteurPremierJoint.ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0,
-                                                      BarreConstant::kTimeoutMs);
-    m_MoteurDeuxiemeJoint.ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0,
-                                                       BarreConstant::kTimeoutMs);
+            m_MoteurPremierJoint.ConfigFeedbackNotContinuous(true) ||
+            m_MoteurDeuxiemeJoint.ConfigFeedbackNotContinuous(true) ||
 
-    m_MoteurPremierJoint.ConfigFeedbackNotContinuous(true);
-    m_MoteurDeuxiemeJoint.ConfigFeedbackNotContinuous(true);
+            SeedEncoder1erJoint() || SeedEncoder2eJoint() ||
 
-    m_MoteurPremierJoint.SetSensorPhase(true);
-    m_MoteurDeuxiemeJoint.SetSensorPhase(false);
+            m_MoteurPremierJoint.SetStatusFramePeriod(StatusFrameEnhanced::Status_13_Base_PIDF0, 10,
+                                                      BarreConstant::kTimeoutMs) ||
+            m_MoteurPremierJoint.SetStatusFramePeriod(StatusFrameEnhanced::Status_10_MotionMagic,
+                                                      10, BarreConstant::kTimeoutMs) ||
+            m_MoteurDeuxiemeJoint.SetStatusFramePeriod(StatusFrameEnhanced::Status_13_Base_PIDF0,
+                                                       10, BarreConstant::kTimeoutMs) ||
+            m_MoteurDeuxiemeJoint.SetStatusFramePeriod(StatusFrameEnhanced::Status_10_MotionMagic,
+                                                       10, BarreConstant::kTimeoutMs) ||
 
-    m_MoteurPremierJoint.SetInverted(true);
-    m_MoteurDeuxiemeJoint.SetInverted(true);
+            m_MoteurPremierJoint.ConfigPeakOutputForward(BarreConstant::kPeakOutputForward,
+                                                         BarreConstant::kTimeoutMs) ||
+            m_MoteurPremierJoint.ConfigPeakOutputReverse(BarreConstant::kPeakOutputReverse,
+                                                         BarreConstant::kTimeoutMs) ||
+            m_MoteurPremierJoint.ConfigNominalOutputForward(BarreConstant::kNominalOutputForward,
+                                                            BarreConstant::kTimeoutMs) ||
+            m_MoteurPremierJoint.ConfigNominalOutputReverse(BarreConstant::kNominalOutputReverse,
+                                                            BarreConstant::kTimeoutMs) ||
 
-    SeedEncoder1erJoint();
-    SeedEncoder2eJoint();
+            m_MoteurDeuxiemeJoint.ConfigPeakOutputForward(BarreConstant::kPeakOutputForward,
+                                                          BarreConstant::kTimeoutMs) ||
+            m_MoteurDeuxiemeJoint.ConfigPeakOutputReverse(BarreConstant::kPeakOutputReverse,
+                                                          BarreConstant::kTimeoutMs) ||
+            m_MoteurDeuxiemeJoint.ConfigNominalOutputForward(BarreConstant::kNominalOutputForward,
+                                                             BarreConstant::kTimeoutMs) ||
+            m_MoteurDeuxiemeJoint.ConfigNominalOutputReverse(BarreConstant::kNominalOutputReverse,
+                                                             BarreConstant::kTimeoutMs) ||
 
-    m_MoteurPremierJoint.SetStatusFramePeriod(StatusFrameEnhanced::Status_13_Base_PIDF0, 10,
-                                              BarreConstant::kTimeoutMs);
-    m_MoteurPremierJoint.SetStatusFramePeriod(StatusFrameEnhanced::Status_10_MotionMagic, 10,
-                                              BarreConstant::kTimeoutMs);
-    m_MoteurDeuxiemeJoint.SetStatusFramePeriod(StatusFrameEnhanced::Status_13_Base_PIDF0, 10,
-                                               BarreConstant::kTimeoutMs);
-    m_MoteurDeuxiemeJoint.SetStatusFramePeriod(StatusFrameEnhanced::Status_10_MotionMagic, 10,
-                                               BarreConstant::kTimeoutMs);
+            m_MoteurPremierJoint.SelectProfileSlot(0, 0) || // Motion
+            m_MoteurPremierJoint.Config_kP(0, BarreConstant::kPMotion1erJoint,
+                                           BarreConstant::kTimeoutMs) ||
+            m_MoteurPremierJoint.Config_kI(0, BarreConstant::kIMotion1erJoint,
+                                           BarreConstant::kTimeoutMs) ||
+            m_MoteurPremierJoint.Config_kD(0, BarreConstant::kDMotion1erJoint,
+                                           BarreConstant::kTimeoutMs) ||
+            m_MoteurPremierJoint.Config_kF(0, BarreConstant::kFMotion1erJoint,
+                                           BarreConstant::kTimeoutMs) ||
 
-    m_MoteurPremierJoint.ConfigPeakOutputForward(BarreConstant::kPeakOutputForward,
-                                                 BarreConstant::kTimeoutMs);
-    m_MoteurPremierJoint.ConfigPeakOutputReverse(BarreConstant::kPeakOutputReverse,
-                                                 BarreConstant::kTimeoutMs);
-    m_MoteurPremierJoint.ConfigNominalOutputForward(BarreConstant::kNominalOutputForward,
-                                                    BarreConstant::kTimeoutMs);
-    m_MoteurPremierJoint.ConfigNominalOutputReverse(BarreConstant::kNominalOutputReverse,
-                                                    BarreConstant::kTimeoutMs);
+            m_MoteurDeuxiemeJoint.SelectProfileSlot(0, 0) || // Motion
+            m_MoteurDeuxiemeJoint.Config_kP(0, BarreConstant::kPMotion2eJoint,
+                                            BarreConstant::kTimeoutMs) ||
+            m_MoteurDeuxiemeJoint.Config_kI(0, BarreConstant::kIMotion2eJoint,
+                                            BarreConstant::kTimeoutMs) ||
+            m_MoteurDeuxiemeJoint.Config_kD(0, BarreConstant::kDMotion2eJoint,
+                                            BarreConstant::kTimeoutMs) ||
+            m_MoteurDeuxiemeJoint.Config_kF(0, BarreConstant::kFMotion2eJoint,
+                                            BarreConstant::kTimeoutMs) ||
 
-    m_MoteurDeuxiemeJoint.ConfigPeakOutputForward(BarreConstant::kPeakOutputForward,
-                                                  BarreConstant::kTimeoutMs);
-    m_MoteurDeuxiemeJoint.ConfigPeakOutputReverse(BarreConstant::kPeakOutputReverse,
-                                                  BarreConstant::kTimeoutMs);
-    m_MoteurDeuxiemeJoint.ConfigNominalOutputForward(BarreConstant::kNominalOutputForward,
-                                                     BarreConstant::kTimeoutMs);
-    m_MoteurDeuxiemeJoint.ConfigNominalOutputReverse(BarreConstant::kNominalOutputReverse,
-                                                     BarreConstant::kTimeoutMs);
+            m_MoteurPremierJoint.ConfigMotionCruiseVelocity(
+                frc::Preferences::GetDouble("kVitesse1erJoint") /
+                BarreConstant::FConversionFactorVelocity1erJoint) ||
+            m_MoteurPremierJoint.ConfigMotionAcceleration(
+                frc::Preferences::GetDouble("kAcceleration1erJoint") /
+                BarreConstant::FConversionFactorAcceleration1erJoint) ||
 
-    m_MoteurPremierJoint.SelectProfileSlot(0, 0); // Motion
-    m_MoteurPremierJoint.Config_kP(0, BarreConstant::kPMotion1erJoint, BarreConstant::kTimeoutMs);
-    m_MoteurPremierJoint.Config_kI(0, BarreConstant::kIMotion1erJoint, BarreConstant::kTimeoutMs);
-    m_MoteurPremierJoint.Config_kD(0, BarreConstant::kDMotion1erJoint, BarreConstant::kTimeoutMs);
-    m_MoteurPremierJoint.Config_kF(0, BarreConstant::kFMotion1erJoint, BarreConstant::kTimeoutMs);
+            m_MoteurDeuxiemeJoint.ConfigMotionCruiseVelocity(
+                frc::Preferences::GetDouble("kVitesse2eJoint") /
+                BarreConstant::FConversionFactorVelocity2eJoint);
+        m_MoteurDeuxiemeJoint.ConfigMotionAcceleration(
+            frc::Preferences::GetDouble("kAcceleration2eJoint") /
+            BarreConstant::FConversionFactorAcceleration2eJoint) ||
 
-    m_MoteurDeuxiemeJoint.SelectProfileSlot(0, 0); // Motion
-    m_MoteurDeuxiemeJoint.Config_kP(0, BarreConstant::kPMotion2eJoint, BarreConstant::kTimeoutMs);
-    m_MoteurDeuxiemeJoint.Config_kI(0, BarreConstant::kIMotion2eJoint, BarreConstant::kTimeoutMs);
-    m_MoteurDeuxiemeJoint.Config_kD(0, BarreConstant::kDMotion2eJoint, BarreConstant::kTimeoutMs);
-    m_MoteurDeuxiemeJoint.Config_kF(0, BarreConstant::kFMotion2eJoint, BarreConstant::kTimeoutMs);
+        m_MoteurPremierJoint.ConfigVoltageCompSaturation(BarreConstant::kVoltageCompensation) ||
 
-    m_MoteurPremierJoint.ConfigMotionCruiseVelocity(
-        frc::Preferences::GetDouble("kVitesse1erJoint") /
-        BarreConstant::FConversionFactorVelocity1erJoint);
-    m_MoteurPremierJoint.ConfigMotionAcceleration(
-        frc::Preferences::GetDouble("kAcceleration1erJoint") /
-        BarreConstant::FConversionFactorAcceleration1erJoint);
+        m_MoteurDeuxiemeJoint.ConfigVoltageCompSaturation(BarreConstant::kVoltageCompensation) ||
 
-    m_MoteurDeuxiemeJoint.ConfigMotionCruiseVelocity(
-        frc::Preferences::GetDouble("kVitesse2eJoint") /
-        BarreConstant::FConversionFactorVelocity2eJoint);
-    m_MoteurDeuxiemeJoint.ConfigMotionAcceleration(
-        frc::Preferences::GetDouble("kAcceleration2eJoint") /
-        BarreConstant::FConversionFactorAcceleration2eJoint);
+        m_MoteurPremierJoint.ConfigPeakCurrentLimit(BarreConstant::kPeakCurrentLimit) ||
+        m_MoteurPremierJoint.ConfigPeakCurrentLimit(BarreConstant::kPeakCurrentDuration) ||
+        m_MoteurPremierJoint.ConfigContinuousCurrentLimit(BarreConstant::kContinuousCurrent) ||
 
-    m_MoteurPremierJoint.ConfigVoltageCompSaturation(BarreConstant::kVoltageCompensation);
-    m_MoteurPremierJoint.EnableVoltageCompensation(true);
+        m_MoteurDeuxiemeJoint.ConfigPeakCurrentLimit(BarreConstant::kPeakCurrentLimit) ||
+        m_MoteurDeuxiemeJoint.ConfigPeakCurrentLimit(BarreConstant::kPeakCurrentDuration) ||
+        m_MoteurDeuxiemeJoint.ConfigContinuousCurrentLimit(BarreConstant::kContinuousCurrent) ||
 
-    m_MoteurDeuxiemeJoint.ConfigVoltageCompSaturation(BarreConstant::kVoltageCompensation);
-    m_MoteurDeuxiemeJoint.EnableVoltageCompensation(true);
+        m_MoteurPremierJoint.ConfigForwardSoftLimitThreshold(
+            BarreConstant::kForwardSoftLimit1erJoint /
+            BarreConstant::FConversionFactorPosition1erJoint) ||
+        m_MoteurPremierJoint.ConfigReverseSoftLimitThreshold(
+            BarreConstant::kReverseSoftLimit1erJoint /
+            BarreConstant::FConversionFactorPosition1erJoint) ||
+        m_MoteurPremierJoint.ConfigForwardSoftLimitEnable(true) ||
+        m_MoteurPremierJoint.ConfigReverseSoftLimitEnable(true) ||
 
-    m_MoteurPremierJoint.ConfigPeakCurrentLimit(BarreConstant::kPeakCurrentLimit);
-    m_MoteurPremierJoint.ConfigPeakCurrentLimit(BarreConstant::kPeakCurrentDuration);
-    m_MoteurPremierJoint.ConfigContinuousCurrentLimit(BarreConstant::kContinuousCurrent);
-    m_MoteurPremierJoint.EnableCurrentLimit(true);
+        m_MoteurDeuxiemeJoint.ConfigForwardSoftLimitThreshold(
+            BarreConstant::kForwardSoftLimit2eJoint /
+            BarreConstant::FConversionFactorPosition2eJoint) ||
+        m_MoteurDeuxiemeJoint.ConfigReverseSoftLimitThreshold(
+            BarreConstant::kReverseSoftLimit2eJoint /
+            BarreConstant::FConversionFactorPosition2eJoint) ||
+        m_MoteurDeuxiemeJoint.ConfigForwardSoftLimitEnable(true) ||
+        m_MoteurDeuxiemeJoint.ConfigReverseSoftLimitEnable(true)) {
+        return;
+    } else {
+        // Stuff that doesn't return a status code
+        m_MoteurPremierJoint.EnableVoltageCompensation(true);
+        m_MoteurDeuxiemeJoint.EnableVoltageCompensation(true);
 
-    m_MoteurDeuxiemeJoint.ConfigPeakCurrentLimit(BarreConstant::kPeakCurrentLimit);
-    m_MoteurDeuxiemeJoint.ConfigPeakCurrentLimit(BarreConstant::kPeakCurrentDuration);
-    m_MoteurDeuxiemeJoint.ConfigContinuousCurrentLimit(BarreConstant::kContinuousCurrent);
-    m_MoteurDeuxiemeJoint.EnableCurrentLimit(true);
+        m_MoteurPremierJoint.EnableCurrentLimit(true);
+        m_MoteurDeuxiemeJoint.EnableCurrentLimit(true);
 
-    m_MoteurPremierJoint.ConfigForwardSoftLimitThreshold(
-        BarreConstant::kForwardSoftLimit1erJoint /
-        BarreConstant::FConversionFactorPosition1erJoint);
-    m_MoteurPremierJoint.ConfigReverseSoftLimitThreshold(
-        BarreConstant::kReverseSoftLimit1erJoint /
-        BarreConstant::FConversionFactorPosition1erJoint);
-    m_MoteurPremierJoint.ConfigForwardSoftLimitEnable(true);
-    m_MoteurPremierJoint.ConfigReverseSoftLimitEnable(true);
+        m_MoteurPremierJoint.SetSensorPhase(true);
+        m_MoteurDeuxiemeJoint.SetSensorPhase(false);
 
-    m_MoteurDeuxiemeJoint.ConfigForwardSoftLimitThreshold(
-        BarreConstant::kForwardSoftLimit2eJoint / BarreConstant::FConversionFactorPosition2eJoint);
-    m_MoteurDeuxiemeJoint.ConfigReverseSoftLimitThreshold(
-        BarreConstant::kReverseSoftLimit2eJoint / BarreConstant::FConversionFactorPosition2eJoint);
-    m_MoteurDeuxiemeJoint.ConfigForwardSoftLimitEnable(true);
-    m_MoteurDeuxiemeJoint.ConfigReverseSoftLimitEnable(true);
+        m_MoteurPremierJoint.SetInverted(true);
+        m_MoteurDeuxiemeJoint.SetInverted(true);
+
+        m_MoteurPremierJoint.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
+        m_MoteurDeuxiemeJoint.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
+
+        m_MotorsInitialized = true;
+    }
 }
 
 void Barre::Periodic() {
@@ -186,7 +203,7 @@ bool Barre::Is2eJointAtTargetAngle(double target) {
     return false;
 }
 
-void Barre::SeedEncoder1erJoint() {
+ctre::phoenix::ErrorCode Barre::SeedEncoder1erJoint() {
 
     int absoluteEncoderPositionPremier{
         m_MoteurPremierJoint.GetSensorCollection().GetPulseWidthPosition()};
@@ -203,10 +220,10 @@ void Barre::SeedEncoder1erJoint() {
         relativeEncoderPositionPremier = relativeEncoderPositionPremier + 4096;
     }
     relativeEncoderPositionPremier = static_cast<int>(relativeEncoderPositionPremier) % 4096;
-    m_MoteurPremierJoint.SetSelectedSensorPosition(relativeEncoderPositionPremier);
+    return m_MoteurPremierJoint.SetSelectedSensorPosition(relativeEncoderPositionPremier);
 }
 
-void Barre::SeedEncoder2eJoint() {
+ctre::phoenix::ErrorCode Barre::SeedEncoder2eJoint() {
     int absoluteEncoderPositionDeuxieme{
         -m_MoteurDeuxiemeJoint.GetSensorCollection().GetPulseWidthPosition()};
     absoluteEncoderPositionDeuxieme = absoluteEncoderPositionDeuxieme % 4096;
@@ -222,7 +239,7 @@ void Barre::SeedEncoder2eJoint() {
         relativeEncoderPositionDeuxieme = relativeEncoderPositionDeuxieme + 4096;
     }
     relativeEncoderPositionDeuxieme = static_cast<int>(relativeEncoderPositionDeuxieme) % 4096;
-    m_MoteurDeuxiemeJoint.SetSelectedSensorPosition(relativeEncoderPositionDeuxieme);
+    return m_MoteurDeuxiemeJoint.SetSelectedSensorPosition(relativeEncoderPositionDeuxieme);
 }
 
 void Barre::KeepCurrentAngle1erJoint() {
