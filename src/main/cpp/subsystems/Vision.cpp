53243b38de091c348f2c538963599b91e4977f99
ASTRO-7605 <144289324+ASTRO-7605@users.noreply.github.com>
2024-02-23 18:17:59 -0500

    Update vision, add NaN checks

--- src/main/cpp/subsystems/Vision.cpp
+++ src/main/cpp/subsystems/Vision.cpp
@@ -1,7 +1,5 @@
 #include "subsystems/Vision.h"
 
-#include <algorithm>
-#include <numbers>
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -18,23 +16,21 @@ Vision::Vision(std::string_view table_name, frc::Transform3d cameraPose)
     camera = (m_PhotonPoseEstimator.GetCamera());
 }
 
-void Vision::Periodic() {
-    // photon::MultiTargetPNPResult multiResult = camera.GetLatestResult().MultiTagResult();
-    // // photon::PhotonTrackedTarget singleResult = m_Vision.GetLatestResult().GetBestTarget();
-    // if (((multiResult.result.isPresent) &&
-    //      (0 < multiResult.result.ambiguity) && (multiResult.result.ambiguity <
-    //      VisionConstant::ambiguityThreshold)) /*|| ((!multiResult.result.isPresent) && (0 <
-    //      singleResult.GetPoseAmbiguity()) && (singleResult.GetPoseAmbiguity() <
-    //      VisionConstant::ambiguityThreshold))*/) {
-    //     auto result = m_PhotonPoseEstimator.Update();
-    //     SendRobotPoseEstimate(result, multiResult);
-    // }
-    isLatestMultiResultValid = false;
-    isLatestSingleResultValid = false;
+void Vision::Periodic() {}
+
+void Vision::UpdateRobotPoseEstimate() {
+    // Invalid if not initialized
+    photon::MultiTargetPNPResult latestMultiResult;
+    photon::PhotonTrackedTarget latestSingleResult;
+
+    bool isLatestMultiResultValid = false;
+    bool isLatestSingleResultValid = true;
+
     if (camera->GetLatestResult().HasTargets()) {
-        lastAprilTagSeen = camera->GetLatestResult().GetBestTarget().GetFiducialId();
         latestMultiResult = camera->GetLatestResult().MultiTagResult();
         latestSingleResult = camera->GetLatestResult().GetBestTarget();
+        lastAprilTagSeen = latestSingleResult.GetFiducialId();
+
         if (camera->GetLatestResult().MultiTagResult().result.isPresent) {
             if (camera->GetLatestResult().MultiTagResult().result.ambiguity <
                 VisionConstant::ambiguityThreshold) {
@@ -45,74 +41,47 @@ void Vision::Periodic() {
             isLatestSingleResultValid = true;
         }
     }
-}
 
-// double Vision::XError() { return m_Vision->GetNumber("tx", 0.0); }
-
-// double Vision::YError() { return m_Vision->GetNumber("ty", 0.0); }
-
-bool Vision::SeesValidTarget() { return (isLatestMultiResultValid || isLatestSingleResultValid); }
-
-// int Vision::ViewTagID() {
-//     // returns a double, but needs to be an int
-//     // return static_cast<int>(m_Vision->GetNumber("tid", 0.0));
-// }
-
-// void Vision::SetPipeline(int pipeline) { m_Vision->PutNumber("pipeline", pipeline); }
-
-// PoseMeasurement Vision::GetRobotPoseEstimate() {
-//     // position in Pathweaver's coord system
-//     // X, Y, Z, roll, pitch, yaw, latency(ms)
-//     std::vector<double> coords =
-//         m_Vision->GetNumberArray("botpose_wpiblue", std::array<double, 7>{});
-
-//     std::vector<double> target_coords =
-//         m_Vision->GetNumberArray("camerapose_targetspace", std::array<double, 6>{});
-
-//     double target_distance = std::sqrt(target_coords[0] * target_coords[0] + // X
-//                                        target_coords[1] * target_coords[1] + // Y
-//                                        target_coords[2] * target_coords[2]   // Z
-//     );
-
-//     PoseMeasurement return_val{
-//         frc::Pose3d{frc::Translation3d{units::meter_t{coords[0]}, units::meter_t{coords[1]},
-//                                        units::meter_t{coords[2]}},
-
-//                     frc::Rotation3d{units::degree_t{coords[3]}, units::degree_t{coords[4]},
-//                                     units::degree_t{coords[5]}}},
-
-//         units::millisecond_t{coords[6]},
-
-//         units::meter_t{target_distance}};
-
-//     return return_val;
-// }
-
-std::optional<PoseMeasurement> Vision::GetRobotPoseEstimate() {
     auto poseEstimate = m_PhotonPoseEstimator.Update();
-    if (!poseEstimate.has_value())
-        return {};
-
-    if (isLatestMultiResultValid) {
-        target_distance = GetAprilTagDistanceMeters(latestMultiResult.result.best.X().value(),
-                                                    latestMultiResult.result.best.Y().value(),
-                                                    latestMultiResult.result.best.Z().value());
-        target_ambiguity = latestMultiResult.result.ambiguity;
-    } else {
-        target_distance =
-            GetAprilTagDistanceMeters(latestSingleResult.bestCameraToTarget.X().value(),
-                                      latestSingleResult.bestCameraToTarget.Y().value(),
-                                      latestSingleResult.bestCameraToTarget.Z().value());
-        target_ambiguity = latestSingleResult.poseAmbiguity;
+    if (!poseEstimate.has_value()) {
+        latestMeasurement = {};
+        return;
     }
 
-    PoseMeasurement return_val{poseEstimate->estimatedPose, poseEstimate->timestamp,
-                               units::meter_t{target_distance}, target_ambiguity};
-    return return_val;
+    if (isLatestMultiResultValid) {
+        target_distance = PythagorasXYZ(latestMultiResult.result.best.X().value(),
+                                        latestMultiResult.result.best.Y().value(),
+                                        latestMultiResult.result.best.Z().value());
+        target_ambiguity = latestMultiResult.result.ambiguity;
+        latestMeasurement = {poseEstimate->estimatedPose, poseEstimate->timestamp,
+                             units::meter_t{target_distance}, target_ambiguity};
+
+    } else if (isLatestSingleResultValid) {
+        target_distance = PythagorasXYZ(latestSingleResult.bestCameraToTarget.X().value(),
+                                        latestSingleResult.bestCameraToTarget.Y().value(),
+                                        latestSingleResult.bestCameraToTarget.Z().value());
+        target_ambiguity = latestSingleResult.poseAmbiguity;
+        latestMeasurement = {poseEstimate->estimatedPose, poseEstimate->timestamp,
+                             units::meter_t{target_distance}, target_ambiguity};
+
+    } else {
+        latestMeasurement = {};
+    }
 }
 
-double Vision::GetAprilTagDistanceMeters(double XDistance, double YDistance, double ZDistance) {
+std::optional<PoseMeasurement> Vision::GetRobotPoseEstimate() {
+    // quick check for NaNs
+    if (latestMeasurement.has_value()) {
+        if (std::isnan(latestMeasurement->pose.X().value()) ||
+            std::isnan(latestMeasurement->pose.Y().value()) ||
+            std::isnan(latestMeasurement->pose.Z().value())) {
+            return {};
+        }
+        return latestMeasurement;
+    }
+    return {};
+}
+
+double Vision::PythagorasXYZ(double XDistance, double YDistance, double ZDistance) {
     return std::sqrt(XDistance * XDistance + YDistance * YDistance + ZDistance * ZDistance);
 }
-
-int Vision::GetAprilTagIDInView() { return lastAprilTagSeen; }
